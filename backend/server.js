69588c06ad7f7aa7914b88ab1dd944883d762bd7
unknown <jonathant11001@gmail.com>
2025-05-19 00:27:00 -0400

    fixing deployment

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:4173",
-  "https://subtrackerfrontend.onrender.com/",
+  "https://subtrackerfrontend.onrender.com",
 ];
 
 // Middleware
